40e5e7020a42b1700b863516b93353cfa49b6ad5
billbillbilly <xiaohaoy@umich.edu>
2024-11-26 21:57:10 -0500

    update test.py

--- csv2jsonl.py
+++
@@ -1,3 +0,0 @@
-import os
-import argparse
-from utils.utils import loadInstruction, const_jsonl
--- test.py
+++ test.py
@@ -5,35 +5,48 @@ from utils.progressbar import printProgressBar
 from utils.key import apikey
 from openai import OpenAI
 import datetime
-from utils.utils import getResponse
+from utils.utils import getResponse, loadInstruction
 
 '''
 ref - https://onlinelibrary.wiley.com/doi/full/10.1002/jrsm.1715
 '''
 
-def use_model(client, df, model, times, threshold, temperature, top_p, test):
+def use_model(client, df, model, times, threshold, temperature, top_p, 
+			  test, iscsv=False, instruction=None, indexCol=None, contentCol=None, 
+			  labelCol=None):
 	l = len(df)
 	colnames = None
 	res = []
 	if test:
 		colnames = ['content', 'answer'] + [f'response_{i}' for i in range(times)] + ['final','TP','TN','FP','FN'] 
 	else:
-		colnames = ['index','content', 'answer'] + [f'response_{i}' for i in range(times)] + ['final']
+		colnames = ['index', 'content'] + [f'response_{i}' for i in range(times)] + ['final']
 	
 	printProgressBar(0, l, prefix = 'Progress:', suffix = 'Complete', length = 50)
 	for index in range(len(df)):
 		row = []
 		final = 'no'
-		tp = 0
-		tn = 0
-		fp = 0
-		fn = 0
-		row_data = df.iloc[index].iloc[0]
-		system = row_data[0]
-		user = row_data[1]
-		content = [user['content']]
 		if test:
-			answer = row_data[2]['content'].lower()
+			tp = 0
+			tn = 0
+			fp = 0
+			fn = 0
+		
+		if iscsv:
+			row_data = df.iloc[index]
+			system = {"role": "system", "content": instruction}
+			user = {"role": "user", "content": row_data[contentCol]}
+			if test:
+				answer = row_data[contentCol].lower()
+			else:
+				ind = row_data[indexCol]
+		else:
+			row_data = df.iloc[index].iloc[0]
+			system = row_data[0]
+			user = row_data[1]
+			content = [user['content']]
+			if test:
+				answer = row_data[2]['content'].lower()
 
 		responses, y_perc = getResponse(client, model, 
 								   system, user, 
@@ -55,7 +68,7 @@ def use_model(client, df, model, times, threshold, temperature, top_p, test):
 		if test:
 			row = [content, answer] + responses + [final,tp,tn,fp,fn]
 		else: 
-			row = [content] + responses + [final]
+			row = [ind, content] + responses + [final]
 		res += [row]
 		printProgressBar(index+1, l, prefix = 'Progress:', suffix = 'Complete', length = 50)
 	res = pd.DataFrame(res, columns=colnames)
@@ -134,7 +147,7 @@ parser.add_argument('--dataRoot',
 parser.add_argument('--testdata', 
                     type=str, 
                     default='', 
-                    help='jsonl file')
+                    help='jsonl/csv file')
 parser.add_argument('--checkpoint', 
                     type=str, 
                     default='', 
@@ -159,6 +172,15 @@ parser.add_argument('--goal',
                     type=str, 
                     default='test', 
                     help='for test or predict')
+parser.add_argument('--colnames', 
+                    type=str, 
+					nargs='+',
+                    default=['index', 'comment', 'label'], 
+                    help='column names for dataset (for prediction puepose or csv input only)')
+parser.add_argument('--instruction', 
+                    type=str, 
+                    default='', 
+                    help='the file name of instruction for testing (for prediction puepose or csv input only)')
 
 args = parser.parse_args()
 
@@ -173,7 +195,16 @@ client = OpenAI(api_key=apikey())
 
 #--------------------load data------------------------
 
-df = pd.read_json(args.testdata, lines=True)
+iscsv = False
+instruction = None
+if ".jsonl" in args.testdata:
+	df = pd.read_json(args.testdata, lines=True)
+elif ".csv" in args.testdata:
+	df = pd.read_csv(args.testdata)
+	df = df.sample(frac = 1)
+	iscsv = True
+	instruction, instruction_ = loadInstruction(args.instruction, args.instruction)
+
 
 #--------------------request GPT------------------------
 
@@ -182,7 +213,9 @@ if args.goal != 'test':
 	test = False
 out = use_model(client, df, model=args.checkpoint, times=args.times, 
 				threshold=args.threshold, temperature=args.temp, 
-				top_p=args.topp, test=test)
+				top_p=args.topp, test=test, iscsv=iscsv, 
+				instruction=instruction, indexCol=args.colnames[0], 
+				contentCol=args.colnames[1], labelCol=args.colnames[2])
 
 #--------------------calculate metrics------------------------
 
